11edc8e5ab83baefb884a7f6db6e7e8b65cc497f
Ptony <filiusancillae@gmail.com>
2026-04-01 13:30:51 -0400

    Just starting

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+import pandas as pd
+
 def main():
-    print("Hello from sts2-2-csv!")
+    # SETUP DATAFRAMES
+    df_elo = pd.DataFrame({
+        "Card":[],
+        "Character":[],
+        "Elo":[],
+        "Elo_win_only":[],
+        "Elo_Act1":[],
+        "Elo_Act2":[],
+        "Elo_Act3":[],
+        "TtlOffers":[]
+    })
+    
+    # Loop through files
+    with os.scandir("Imports") as entries:
+        for entry in entries:
+            if entry.name.find(".run") == -1:
+                continue
+
 
 
 if __name__ == "__main__":
